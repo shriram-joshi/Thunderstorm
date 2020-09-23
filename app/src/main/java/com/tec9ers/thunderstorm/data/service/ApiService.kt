5e69a1168025ad0d3c40c146ce85f51efae5af8c
unc0ded <hershkumar01@gmail.com>
2020-09-23 14:28:34 +0530

    Added a test API call for OneCallAPI in MainActivity

--- app/src/main/java/com/tec9ers/thunderstorm/data/service/ApiService.kt
+++ app/src/main/java/com/tec9ers/thunderstorm/data/service/ApiService.kt
@@ -2,6 +2,7 @@ package com.tec9ers.thunderstorm.data.service
 
 import com.tec9ers.thunderstorm.model.CurrentWeatherResponse
 import com.tec9ers.thunderstorm.model.ForecastResponse
+import com.tec9ers.thunderstorm.model.onecallapi.OneCallAPIResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
@@ -12,6 +13,8 @@ interface ApiService {
     fun getCurrentWeather(@QueryMap map: HashMap<String,String>) : Single<CurrentWeatherResponse>
 
     @GET("forecast")
-    fun getForecast(map: HashMap<String, String>) : Single<ForecastResponse>
+    fun getForecast(@QueryMap map: HashMap<String, String>) : Single<ForecastResponse>
 
+    @GET("onecall")
+    fun getOneCallApiData(@QueryMap map: HashMap<String, String>) : Single<OneCallAPIResponse>
 }
