50312e623f45517bde9a5ecb933afb178911ef46
Aditya Kurkure <56596662+epicadk@users.noreply.github.com>
2020-10-17 15:33:40 +0530

    Landing Fragment (#13)
    * Added Hourly Forecast RecyclerView

---
+++ app/src/main/java/com/tec9ers/thunderstorm/view/adapter/HourlyForecastRecyclerViewAdapter.kt
@@ -0,0 +1,47 @@
+package com.tec9ers.thunderstorm.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tec9ers.thunderstorm.R
+import com.tec9ers.thunderstorm.model.onecallapi.Hourly
+import com.tec9ers.thunderstorm.utils.FormatUtils
+import dagger.hilt.android.qualifiers.ActivityContext
+import kotlinx.android.synthetic.main.item_hourly_forecast.view.*
+import javax.inject.Inject
+
+@ActivityContext
+class HourlyForecastRecyclerViewAdapter @Inject constructor(@ActivityContext val context: Context, val format: FormatUtils) :
+    RecyclerView.Adapter<HourlyForecastRecyclerViewAdapter.ForecastViewHolder>() {
+
+    private var data: List<Hourly>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.item_hourly_forecast, parent, false)
+        return ForecastViewHolder(view)
+    }
+    fun setData(data: List<Hourly>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+    override fun getItemCount(): Int {
+        return if (data == null) 0 else data!!.size
+    }
+
+    override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
+        data?.get(position)?.let { holder.setData(it,format) }
+    }
+
+    class ForecastViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun setData(hourly: Hourly, format: FormatUtils) {
+            with(itemView) {
+                tv_forecast_hourly_desc.text = hourly.weather[0].description
+                tv_forecast_hourly_temp.text = format.formatTemp(hourly.temp)
+                tv_forecast_hourly_time.text = format.formatTime(hourly.dt)
+            }
+        }
+    }
+}
--- app/src/main/java/com/tec9ers/thunderstorm/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/tec9ers/thunderstorm/view/fragment/HomeFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tec9ers.thunderstorm.R
+import com.tec9ers.thunderstorm.model.onecallapi.OneCallAPIResponse
 import com.tec9ers.thunderstorm.utils.FormatUtils
+import com.tec9ers.thunderstorm.view.adapter.HourlyForecastRecyclerViewAdapter
 import com.tec9ers.thunderstorm.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -19,31 +23,24 @@ class HomeFragment : Fragment() {
     private val homeViewModel: HomeViewModel by viewModels()
     @Inject
     lateinit var formatUtils :FormatUtils
+    @Inject
+    lateinit var hourlyForecastRecyclerViewAdapter: HourlyForecastRecyclerViewAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rv_today_forecast.adapter = hourlyForecastRecyclerViewAdapter
+        rv_today_forecast.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel.oneCallApiLiveData.observe(
-            viewLifecycleOwner,
-            { oneCallAPIResponse ->
-                oneCallAPIResponse?.apply {
-                    with(formatUtils) {
-                        tv_feels_like_temp.text = formatTemp(current.feelsLike)
-                        tv_temp_main.text = formatTemp(current.temp)
-                        tv_wind_speed.text = formatWindSpeed(current.windSpeed, current.windDeg)
-                        tv_weather_condition.text = current.weather[0].description
-                        tv_sun_rise.text = formatTime(current.sunrise)
-                        tv_sun_set.text = formatTime(current.sunset)
-                        tv_uv_index.text = formatUVI(current.uvi)
-                        tv_pressure.text  = formatPressure(current.pressure)
-                        tv_humidity.text  = formatHumidity(current.humidity)
-                        tv_visibility.text = formatVisibility(current.visibility)
-                    }
-                }
-
-            })
+        homeViewModel.oneCallApiLivaData.observe(viewLifecycleOwner) { oneCallAPIResponse ->
+            setData(oneCallAPIResponse)
+        }
 
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
@@ -66,5 +63,23 @@ class HomeFragment : Fragment() {
             else -> ""
         }
         return getString(R.string.wind_speed, windSpeed) + " " + dir
+    }
+
+    private fun setData(oneCallAPIResponse: OneCallAPIResponse) {
+        oneCallAPIResponse.apply {
+            with(formatUtils) {
+                tv_feels_like_temp.text = formatTemp(current.feelsLike)
+                tv_temp_main.text = formatTemp(current.temp)
+                tv_wind_speed.text = formatWindSpeed(current.windSpeed, current.windDeg)
+                tv_weather_condition.text = current.weather[0].description
+                tv_sun_rise.text = formatTime(current.sunrise)
+                tv_sun_set.text = formatTime(current.sunset)
+                tv_uv_index.text = formatUVI(current.uvi)
+                tv_pressure.text = formatPressure(current.pressure)
+                tv_humidity.text = formatHumidity(current.humidity)
+                tv_visibility.text = formatVisibility(current.visibility)
+                hourlyForecastRecyclerViewAdapter.setData(hourly)
+            }
+        }
     }
 }
