bed7f3d33cb49524c3ef0c6a053c3f5ca3df591a
Epicadk <adityakurkure@gmail.com>
2020-10-23 22:30:42 +0530

    Added Logic to Fav cities fragment

--- app/src/main/java/com/tec9ers/thunderstorm/view/adapter/FavCitiesAdapter.kt
+++ app/src/main/java/com/tec9ers/thunderstorm/view/adapter/FavCitiesAdapter.kt
@@ -4,18 +4,18 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.tec9ers.thunderstorm.R
 import com.tec9ers.thunderstorm.model.CurrentWeatherResponse
-import com.tec9ers.thunderstorm.model.onecallapi.Daily
 import com.tec9ers.thunderstorm.utils.FormatUtils
 import dagger.hilt.android.qualifiers.ActivityContext
-import kotlinx.android.synthetic.main.item_daily_forecast.view.*
 import javax.inject.Inject
 
 @ActivityContext
-class FavCitiesAdapter @Inject constructor(@ActivityContext val context: Context, private val format: FormatUtils) :
+class FavCitiesAdapter @Inject constructor(
+    @ActivityContext val context: Context,
+    private val format: FormatUtils
+) :
     RecyclerView.Adapter<FavCitiesAdapter.FavCitiesViewHolder>() {
 
     private var data: List<CurrentWeatherResponse>? = null
@@ -26,10 +26,12 @@ class FavCitiesAdapter @Inject constructor(@ActivityContext val context: Context
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavCitiesViewHolder {
-        return FavCitiesViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_daily_forecast, parent, false))
+        return FavCitiesViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_daily_forecast, parent, false)
+        )
     }
 
-    override fun onBindViewHolder(holder:FavCitiesViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: FavCitiesViewHolder, position: Int) {
         data?.get(position)?.let { holder.populate(it, format) }
     }
 
--- app/src/main/java/com/tec9ers/thunderstorm/view/fragment/FavCities.kt
+++ app/src/main/java/com/tec9ers/thunderstorm/view/fragment/FavCities.kt
@@ -1,18 +1,55 @@
 package com.tec9ers.thunderstorm.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tec9ers.thunderstorm.R
+import com.tec9ers.thunderstorm.utils.DataStoreUtils
+import com.tec9ers.thunderstorm.view.adapter.FavCitiesAdapter
+import com.tec9ers.thunderstorm.viewmodel.FavCitiesViewModel
+import kotlinx.android.synthetic.main.fragment_fav_cities.*
+import javax.inject.Inject
 
 class FavCities : Fragment() {
+
+    private val viewModel: FavCitiesViewModel by viewModels()
+
+    @Inject
+    lateinit var dataStoreUtils: DataStoreUtils
+
+    @Inject
+    lateinit var adapter: FavCitiesAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        floating_button.setOnClickListener(View.OnClickListener {
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_favCities_to_searchFragment)
+        })
+        rv_fav_cities.adapter = adapter
+        rv_fav_cities.layoutManager = LinearLayoutManager(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        dataStoreUtils.citiesFlow.asLiveData()
+            .observe(viewLifecycleOwner,
+                { citiesList -> setupRecyclerView(citiesList) })
         return inflater.inflate(R.layout.fragment_fav_cities, container, false)
+    }
+
+    private fun setupRecyclerView(citiesList: MutableList<String>) {
+        viewModel.getOneCallApiLiveData(citiesList)
+            .observe(viewLifecycleOwner, { response ->
+                adapter.setData(response)
+            })
     }
 }
